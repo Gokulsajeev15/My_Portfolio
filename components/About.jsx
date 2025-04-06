3d41abea2b009aec5616b36d75c5b427dd40d068
Gokulsajeev15 <gokulsajeev10@gmail.com>
2025-04-06 14:30:50 +0100

    Deployed in pages

--- components/About.jsx
+++ components/About.jsx
@@ -6,10 +6,10 @@ function About() {
             <p>I'm a Student Doing My MSc Computer Science in UCD</p>
             <div className="about-links">
                 <a href="https://github.com/Gokulsajeev15" target="_blank" rel="noopener noreferrer">
-                    <img src="./assets/github-mark.png" alt="GitHub" className="social-icon" />
+                    <img src="/assets/github-mark.png" alt="GitHub" className="social-icon" />
                 </a>
                 <a href="https://www.linkedin.com/in/gokulsajeev/" target="_blank" rel="noopener noreferrer">
-                    <img src="./assets/InBug-Black.png" alt="LinkedIn" className="social-icon-l" />
+                    <img src="/assets/InBug-Black.png" alt="LinkedIn" className="social-icon-l" />
                 </a>
             </div>
         </section>
