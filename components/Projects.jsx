d29dc4afde1d1450a6dbff763625fbfc80146784
Gokulsajeev15 <gokulsajeev10@gmail.com>
2025-05-25 14:46:17 +0100

    Added some projects

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -17,6 +17,14 @@ function ProjectCard({ title, description, image,link }) {
 function Projects() {
 
   const projectData = [
+    
+    {
+      title: 'Agribridge',
+      description: "Agribridge is a platform that bridges the gap between farmers and sellers by enabling the seamless sale of agricultural waste products through a modern, user-friendly web interface",
+      image: '/My_Portfolio/assets/Agribridge.png',
+      link:'https://github.com/Gokulsajeev15/Agribridge'
+      
+    },
     {
       title: 'Automated Survey Scheduler',
       description: 'The Automated Survey Scheduler automates personalized survey invitations with AI-powered email content and seamless Gmail integration. It offers flexible scheduling, participant management, and bulk CSV uploads to optimize survey engagement.',
@@ -24,6 +32,13 @@ function Projects() {
       link:'https://github.com/Gokulsajeev15/Automated-Survey_scheduler'
       
     },
+        {
+      title: 'Finance Assistant',
+      description: "SmartInvest is a domain-specific assistant that empowers users with real-time financial insights about Fortune 500 companies by combining natural language understanding with live stock data, intelligent metric analysis, and seamless PDF-based ticker lookup",
+      image: '/My_Portfolio/assets/Finance assistant.png',
+      link:'https://github.com/Gokulsajeev15/Finance-Assistant'
+      
+    }
 
   ];
 
